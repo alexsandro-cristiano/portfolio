a032f80e48dd00a4cb732644316cf85aebd69f20
Alexsandro Cristiano <acg.alexsandro@hotmail.com>
2022-03-12 14:19:33 -0300

    Adiciona arquivo javascript
    - logica para implementar cartões de projeto mockados

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,58 @@
-const cardData = [
+const projectsData = [
   {
     id: 0,
-    name: 'teste',
-    description: 'teste 1',
-    linkGithub:'l.com',
-    linkWeb: 'g.com'
+    name: "RockSité",
+    description:
+      "Site desenvolvido no 2° periodo da graduação.Utilizado como tema base estilo musical rock n roll.",
+    linkGithub: "https://github.com/alexsandro-cristiano/rocksite",
+    linkWeb: "https://rocksite.netlify.app/",
   },
   {
     id: 1,
-    name: 'teste1',
-    description: 'teste 1',
-    linkGithub:'l.com',
-    linkWeb: 'g.com'
+    name: "Cartão de Visita",
+    description:
+      "Página desenvolvida com a inspiração do layout da NLW Heart. Objetivo de ser meu cartão de visita",
+    linkGithub: "https://github.com/alexsandro-cristiano/cartao-de-visita",
+    linkWeb: "https://rocksite.netlify.app/",
   },
   {
     id: 2,
-    name: 'teste2',
-    description: 'teste 1',
-    linkGithub:'l.com',
-    linkWeb: 'g.com'
+    name: "DS Movie",
+    description:
+      "Catálogo de filmes para ser avaliado, a página foi desenvolvida durante a semana Spring React do professor Nelio Alves.",
+    linkGithub: "https://github.com/alexsandro-cristiano/dsmovie",
+    linkWeb: "https://acmovie.netlify.app/",
   },
-
 ];
 
+function createElementCard(indice) {
+  const currentElement = projectsData[indice];
+  const html = `<div class="card">
+    <h2 class="card__title">${currentElement.name}</h2>
+    <p class="card__description">${currentElement.description}</p>
+    <div class="card__link-container">
+    <a class="card__link" href="${currentElement.linkWeb}">web</a>
+    <a  class="card__link" href="${currentElement.linkGithub}">github</a>
+    </div>
+    </div>`;
+  return html;
+}
 
+function renderProjectCard(indice) {
+  const divContainerCard = document.querySelector(".block-cards");
+  const cardHtml = createElementCard(indice);
+  divContainerCard.insertAdjacentHTML("beforeend", cardHtml);
+}
 
-/*
-
-*/
-function createCard() {
-const html = ``
+function populateHTML() {
+  let i;
+  for (i = 0; i < projectsData.length; i++) {
+    renderProjectCard(i);
+  }
 }
 
 function init() {
-  console.log(cardData);
-
-  const d = document.querySelector('.block-cards')
-  console.log(typeof(d))
-  //d.innerHTML = 'oi'
+  populateHTML();
 }
+
 init();
