99cf179992355af4935dc4f9511092405cab9fd0
Alexsandro Cristiano <acg.alexsandro@hotmail.com>
2022-03-12 10:07:50 -0300

    card for project concluido

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+const cardData = [
+  {
+    id: 0,
+    name: 'teste',
+    description: 'teste 1',
+    linkGithub:'l.com',
+    linkWeb: 'g.com'
+  },
+  {
+    id: 1,
+    name: 'teste1',
+    description: 'teste 1',
+    linkGithub:'l.com',
+    linkWeb: 'g.com'
+  },
+  {
+    id: 2,
+    name: 'teste2',
+    description: 'teste 1',
+    linkGithub:'l.com',
+    linkWeb: 'g.com'
+  },
+
+];
+
+
+
+/*
+
+*/
+function createCard() {
+const html = ``
+}
+
+function init() {
+  console.log(cardData);
+
+  const d = document.querySelector('.block-cards')
+  console.log(typeof(d))
+  //d.innerHTML = 'oi'
+}
+init();
