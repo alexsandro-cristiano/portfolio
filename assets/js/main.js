dbc31db85269126955f5646d7318eb13333dafe1
alexsandro-cristiano <acg.alexsandro@hotmail.com>
2022-04-23 18:53:02 -0300

    conforme manda o padrao - ajustes

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ const projectsData = [
     description:
       'Site desenvolvido no 2° periodo da graduação.Utilizado como tema base estilo musical rock n roll.',
     linkGithub: 'https://github.com/alexsandro-cristiano/rocksite',
-    linkWeb: 'https://rocksite.netlify.app/',
+    linkWeb: 'https://rocksite.netlify.app/'
   },
   {
     id: 1,
@@ -15,7 +15,7 @@ const projectsData = [
     description:
       'Página desenvolvida com a inspiração do layout da NLW Heart. Objetivo de ser meu cartão de visita',
     linkGithub: 'https://github.com/alexsandro-cristiano/cartao-de-visita',
-    linkWeb: 'https://cartaovisita.netlify.app/',
+    linkWeb: 'https://cartaovisita.netlify.app/'
   },
   {
     id: 2,
@@ -24,7 +24,7 @@ const projectsData = [
     description:
       'Catálogo de filmes para ser avaliado, a página foi desenvolvida durante a semana Spring React do professor Nelio Alves.',
     linkGithub: 'https://github.com/alexsandro-cristiano/dsmovie',
-    linkWeb: 'https://acmovie.netlify.app/',
+    linkWeb: 'https://acmovie.netlify.app/'
   },
   {
     id: 3,
@@ -32,8 +32,23 @@ const projectsData = [
     tec: 'html, css, js',
     description: 'Página para apresentar meus projeto pessoais e academicos.',
     linkGithub: 'https://github.com/alexsandro-cristiano/portfolio',
-    linkWeb: 'https://alexsandrodev.netlify.app/',
-  },
+    linkWeb: 'https://alexsandrodev.netlify.app/'
+  }
+]
+
+const tecs = [
+  'react',
+  'bootstrap',
+  'java',
+  'spring boot',
+  'nodejs',
+  'php',
+  'c/c++',
+  'mysql',
+  'postgresql',
+  'git',
+  'github',
+  'uml'
 ]
 
 function createElementCard(indice) {
@@ -47,7 +62,6 @@ function createElementCard(indice) {
                 </i></a>
                 <a class="card__link" target='__black' href="${currentElement.linkWeb}"><i class="icon icon-globe"></i>
                 </a>
-                
               </div>
             </div>
             <div class="card__content">
@@ -66,14 +80,38 @@ function renderProjectCard(indice) {
   divContainerCard.insertAdjacentHTML('beforeend', cardHtml)
 }
 
-function populateHTML() {
+function projetos() {
   let i
   for (i = 0; i < projectsData.length; i++) {
     renderProjectCard(i)
   }
 }
 
+function renderSkillsCard(i) {
+  const divContainerCard = document.querySelector('.block-skills')
+  const html = `
+  <div class="skill">
+    <i class="icon icon-check-square"></i>
+    <p>${tecs[i]}</p>
+  </div>`
+
+  divContainerCard.insertAdjacentHTML('beforeend', html)
+}
+
+function skills() {
+  let i
+  for (i = 0; i < tecs.length; i++) {
+    renderSkillsCard(i)
+  }
+}
+
+function populateHTML() {
+  skills()
+  projetos()
+}
+
 function changeHeaderWhenScroll(header, navHeight) {
+  navHeight -= 52
   if (window.scrollY >= navHeight) {
     header.classList.add('header--show')
   } else {
@@ -83,11 +121,7 @@ function changeHeaderWhenScroll(header, navHeight) {
 
 function handleMenu() {
   const nav = document.querySelector('#header nav')
-  console.log(nav)
-
   const icons = document.querySelectorAll('nav .icon')
-  console.log(icons)
-
   for (const element of icons) {
     element.addEventListener('click', () => {
       nav.classList.toggle('show')
@@ -111,20 +145,6 @@ function init() {
   populateHTML()
   handleMenu()
   handleLinkMenu()
-
-  const scrollReveal = ScrollReveal({
-    origin: 'top',
-    distance: '50px',
-    duration: 700,
-    reset: true,
-  })
-
-  scrollReveal.reveal(
-    `#apresentacao, #apresentacao .title, #apresentacao .subtitle, #apresentacao .description, #apresentacao .midia-social
-    `,
-    { interval: 100 }
-  )
-
   const header = document.querySelector('#header')
   const navHeight = header.offsetHeight
   window.addEventListener('scroll', function () {
