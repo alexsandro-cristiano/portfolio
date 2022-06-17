807b553b75e3477ff909aa7023a3b96debe72b55
alexsandro-cristiano <acg.alexsandro@hotmail.com>
2022-06-17 01:35:47 -0300

    hotfix: corrige responsividade 320x580

--- assets/js/cardskill.js
+++ assets/js/cardskill.js
@@ -1,34 +1,46 @@
-const skills = ["html", "css", "javascript", "react", "bootstrap", "java", "spring boot", "nodejs", "php","c", "mysql", "postgresql", "git", "github", "uml"]
+const skills = [
+  'html',
+  'css',
+  'javascript',
+  'react',
+  'bootstrap',
+  'java',
+  'spring boot',
+  'nodejs',
+  'php',
+  'c',
+  'mysql',
+  'postgresql',
+  'git',
+  'github',
+  'uml'
+]
 
 function createCard(indice) {
-  const div = document.createElement("div");
-  const icon = document.createElement("i");
-  const line = document.createElement("p");
+  const div = document.createElement('div')
+  const icon = document.createElement('i')
+  const line = document.createElement('p')
 
-  div.classList.add("skill");
-  icon.classList.add("icon-badge");
-  icon.classList.add("icon");
+  div.classList.add('skill')
+  icon.classList.add('icon-badge')
+  icon.classList.add('icon')
 
-  line.innerHTML = `${skills[indice]}`;
-  console.log("na funcao create")
-  div.appendChild(icon);
-  div.appendChild(line);
-  
-  return div;
+  line.innerHTML = `${skills[indice]}`
+  div.appendChild(icon)
+  div.appendChild(line)
+
+  return div
 }
 
 export function addCard(indice) {
-  const cd = document.querySelector("#cardSkill");
-  console.log("na funcao addCard")
-  const card = createCard(indice);
-  cd.appendChild(card);
+  const cd = document.querySelector('.js-cardSkill')
+  const card = createCard(indice)
+  cd.appendChild(card)
 }
 
 export function init() {
-  console.log(skills.length)
   let i
   for (i = 0; i < skills.length; i++) {
-    console.log(i)
     addCard(i)
   }
 }
